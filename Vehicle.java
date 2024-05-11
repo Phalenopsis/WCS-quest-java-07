c29ab34096acf45a54b96e4aab95c6ade5ea2911
Nicolas Leroux <hertford.fr@gmail.com>
2024-05-11 16:40:59 +0200

    feat: add Vehicle class

---
+++ Vehicle.java
@@ -0,0 +1,25 @@
+public abstract class Vehicle {
+    String brand;
+    int kilometers;
+
+    public Vehicle (String brand, int kilometers) {
+        this.brand = brand;
+        this.kilometers = kilometers;
+    }
+
+    public int getKilometers() {
+        return kilometers;
+    }
+
+    public void setKilometers(int kilometers) {
+        this.kilometers = kilometers;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+}
